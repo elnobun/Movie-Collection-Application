31c0c4da19d9703fd5aa6010615c8285c3106922
Ellis Enobun <elnobun@gmail.com>
2017-05-04 18:09:36 +0100

    Added the delete cascade to maintain database integrity.

--- moviecollection/database_setup.py
+++ moviecollection/database_setup.py
@@ -1,18 +1,21 @@
+# coding=utf-8
+""""
+Import dependencies
+"""
 import os
 from sqlalchemy import Column as Col, ForeignKey, Integer, String as Str
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker, backref
 from sqlalchemy import create_engine
 
-
 Base = declarative_base()
+
 
 ###############################################################################
 # Tables: User, Collection, Album
 ###############################################################################
 
 class User(Base):
-    __tablename__ = 'user'
     """ Table for user information.
 
     Columns:
@@ -21,6 +24,8 @@ class User(Base):
         email: E-Mail of the user.
         picture: Path to external profile picture.
     """
+    __tablename__ = 'user'
+
     id = Col(Integer, primary_key=True)
     name = Col(Str(250), nullable=False)
     email = Col(Str(250), nullable=False)
@@ -28,7 +33,12 @@ class User(Base):
 
     @property
     def serialize(self):
+        """ Selects and formats collection data.
 
+        This serializable function will help define what data should be
+        sent across and put it in a format that Flask can easily use.
+        """
+        # Returns object data in easily serializeable format
         return {
             'name': self.name,
             'id': self.id,
@@ -38,7 +48,6 @@ class User(Base):
 
 
 class Collection(Base):
-    __tablename__ = 'collection'
     """ Table for movie collections.
 
     Columns:
@@ -46,6 +55,8 @@ class Collection(Base):
         name: Name of the collection.
         user_id: user who created the collection.
     """
+
+    __tablename__ = 'collection'
 
     id = Col(Integer, primary_key=True)
     name = Col(Str(250), nullable=False)
@@ -66,8 +77,8 @@ class Collection(Base):
             'name': self.name
         }
 
+
 class Movie(Base):
-    __tablename__ = 'movie'
     """ Table for movie information.
 
     Columns:
@@ -81,6 +92,7 @@ class Movie(Base):
         user_id: user who created the movie entry.
         collection_id: collections where the movie belongs to.
     """
+    __tablename__ = 'movie'
 
     id = Col(Integer, primary_key=True)
     name = Col(Str(250), nullable=False)
@@ -93,7 +105,7 @@ class Movie(Base):
     user_id = Col(Integer, ForeignKey('user.id'))
     user = relationship(User)
     collection_id = Col(Integer, ForeignKey('collection.id'))
-    collection = relationship(Collection)
+    collection = relationship(Collection, backref=backref('movie', cascade='all, delete'))
 
     # Decorator method
     @property
@@ -124,6 +136,7 @@ database_name = 'moviecollections.db'
 
 use_SQlite = True  # Boolean
 
+
 def get_database_session():
     """Returns a session for executing queries.
 
@@ -146,7 +159,7 @@ def create_all():
     Deletes the database if it already exists and creates a new database in
     its place. The tables are defined in file as classes that inherit a
     ``declarative_base()`` instance.
-    :param echo:
+    :param: echo.
     """
 
     db_name = database_name
@@ -154,6 +167,7 @@ def create_all():
     if use_SQlite:
         engine = create_engine(sqlite_dbapi + db_name)
         Base.metadata.create_all(engine)
+
 
 def drop_all():
     """Deletes all tables from database.
